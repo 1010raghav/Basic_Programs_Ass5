f6e427ffecdcd837c5f93269bf0720c7e726d9fc
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-23 19:12:27 +0530

    Main Program file

---
+++ program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Day3_emp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            HeadTailsPercentage.CoinTossed();
+            LeapYearPercentage.LeapYear();
+            EvenOdd.Evenodd();
+            PowerOF2.powerof2();
+            HarmonicNumber.harmonicNumber();
+            Factors.factor();
+            QuotientAndRemaindercs.QuotientRemainder();
+            Swap2Numbers.Swap();
+            VowelorConsonant.vowel();
+            LargeNumber.largenum();
+        }
+    }
+}
