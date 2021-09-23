0e86e0bba8ec54c30f6875651b15c6ef374792c2
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-23 19:17:41 +0530

    Main Program file

--- program.cs
+++ program.cs
@@ -8,15 +8,45 @@ namespace Day3_emp
         {
            
             HeadTailsPercentage.CoinTossed();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             LeapYearPercentage.LeapYear();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             EvenOdd.Evenodd();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             PowerOF2.powerof2();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             HarmonicNumber.harmonicNumber();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             Factors.factor();
-            QuotientAndRemaindercs.QuotientRemainder();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
+            
+           QuotientAndRemaindercs.QuotientRemainder();
+           Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             Swap2Numbers.Swap();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             VowelorConsonant.vowel();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
+            
             LargeNumber.largenum();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("**********************************************************");
         }
     }
 }
